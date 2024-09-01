1ecccce9c07514afa3ff7346b7db849dde18e15d
Lowen <166390338+lowen-studio@users.noreply.github.com>
2024-09-01 12:52:50 +0100

    Update more-stories.tsx

--- src/app/_components/more-stories.tsx
+++ src/app/_components/more-stories.tsx
@@ -9,9 +9,9 @@ export function MoreStories({ posts }: Props) {
   return (
     <section>
       <h2 className="mb-8 text-5xl md:text-7xl font-bold tracking-tighter leading-tight">
-        
+
       </h2>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-y-20 mb-32">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-20 mb-32">
         {posts.map((post) => (
           <PostPreview
             key={post.slug}
