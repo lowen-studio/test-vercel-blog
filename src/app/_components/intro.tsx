fd0b1be085e5891e2a1700bfb94e8e6f7c4e3ce9
Lowen <166390338+lowen-studio@users.noreply.github.com>
2024-09-01 12:31:45 +0100

    Update intro.tsx

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -3,7 +3,7 @@ import { CMS_NAME } from "@/lib/constants";
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-5xl font-medium tracking-tight leading-tight md:pr-8">
+      <h1 className="text-5xl md:text-5xl font-bold tracking-tight leading-tight md:pr-8">
         Superblog
       </h1>
       <h4 className="text-center md:text-left text-xl mt-5 md:pl-8">
