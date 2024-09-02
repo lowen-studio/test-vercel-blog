e4c99a0ba2d2abce984c36a0217b8c5cd8df3594
Lowen <166390338+lowen-studio@users.noreply.github.com>
2024-09-02 12:10:37 +0100

    Update header.tsx

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <h2 className="text-3xl md:text-6xl font-semibold tracking-tight md:tracking-tight leading-tight mb-20 mt-8">
+    <h2 className="text-4xl md:text-5xl font-semibold tracking-tight md:tracking-tight leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
         Superblog
       </Link>
