6013f35782d554bd20e15426e5c3b72f0d187d63
Lowen <166390338+lowen-studio@users.noreply.github.com>
2024-09-10 19:17:39 +0100

    Update post-header.tsx

--- src/app/_components/post-header.tsx
+++ src/app/_components/post-header.tsx
@@ -14,7 +14,12 @@ type Props = {
 export function PostHeader({ title, coverImage, date, author }: Props) {
   return (
     <>
+      <section className="flex-col md:flex-row flex items-center md:justify-between mt-8 mb-16 md:mb-12">
       <PostTitle>{title}</PostTitle>
+      <h4 className="text-center md:text-left text-xl mt-5 md:pl-8">
+        {excerpt}
+      </h4>
+    </section>
 
       <div className="mb-8 md:mb-16 sm:mx-0">
         <CoverImage title={title} src={coverImage} />
