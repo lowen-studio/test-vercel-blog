854034223c62b6981c34265f3c49b6039b38bd45
Lowen <166390338+lowen-studio@users.noreply.github.com>
2024-09-10 10:14:54 +0100

    Update header.tsx

--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 const Header = () => {
   return (
-    <h2 className="text-lg md:text-xl leading-tight mb-20 mt-8">
+    <h2 className="text-lg leading-tight mb-20 mt-8">
       <Link href="/" className="hover:underline">
         Back to Projects
       </Link>
