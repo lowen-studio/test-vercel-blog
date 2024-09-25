acd7d68374e9ff646a8ae7d280a35ad2fd333714
Lowen <166390338+lowen-studio@users.noreply.github.com>
2024-09-25 09:38:51 +0100

    Update post-title.tsx

--- src/app/_components/post-title.tsx
+++ src/app/_components/post-title.tsx
@@ -6,7 +6,7 @@ type Props = {
 
 export function PostTitle({ children }: Props) {
   return (
-    <h1 className="text-5xl md:text-6xl lg:text-7xl max-w-3xl font-semibold tracking-tight leading-tight md:leading-none mb-12 text-left">
+    <h1 className="text-5xl md:text-6xl lg:text-7xl max-w-3xl font-semibold tracking-tight leading-tight md:leading-none mb-2 text-left">
       {children}
     </h1>
   );
