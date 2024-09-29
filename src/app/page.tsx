30f1f101d770835752ed67302a80f49e11aa93fc
Lowen <166390338+lowen-studio@users.noreply.github.com>
2024-09-29 04:13:04 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,9 +14,17 @@ export default function Index() {
   return (
     <main>
       <Container>
-        <Intro />
-        <MoreStories posts={morePosts} />
-      </Container>
+        <Intro /> 
+        <HeroPost
+          title={heroPost.title}
+          coverImage={heroPost.coverImage}
+          date={heroPost.date}
+          author={heroPost.author}
+          slug={heroPost.slug}
+          excerpt={heroPost.excerpt}
+        />
+        
+        {morePosts.length > 0 && <MoreStories posts={morePosts} />}
     </main>
   );
 }
