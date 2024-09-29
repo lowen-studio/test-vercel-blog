1e1f7f87832be18183feee46f3207e00f023d848
Lowen <166390338+lowen-studio@users.noreply.github.com>
2024-09-29 04:25:01 +0100

    Update hero-post.tsx

--- src/app/_components/hero-post.tsx
+++ src/app/_components/hero-post.tsx
@@ -24,17 +24,17 @@ export function HeroPost({
   return (
     <section>
       <div className="mb-8 md:mb-16">
-        <CoverImage title={title} src={coverImage} slug={slug} />
       </div>
-      <div className="md:grid md:grid-cols-2 md:gap-x-16 lg:gap-x-8 mb-20 md:mb-28">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-12 mb-32">
         <div>
-          <h3 className="mb-4 text-4xl lg:text-5xl font-semibold leading-tight">
+                  <CoverImage title={title} src={coverImage} slug={slug} />
+        </div>
+        <div>
+           <h3 className="mb-4 text-4xl lg:text-5xl font-semibold leading-tight">
             <Link href={`/posts/${slug}`} className="hover:underline">
               {title}
             </Link>
           </h3>
-        </div>
-        <div>
           <p className="text-lg leading-relaxed mb-4">{excerpt}</p>
         </div>
       </div>
